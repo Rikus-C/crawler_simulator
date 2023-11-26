35a139cc6cd7ab0383acc0e67fed51836093762c
Rikus-C <rcoetzee967@gmail.com>
2023-11-26 23:41:55 +0200

    working on froward and backwards

--- crawlers.py
+++ crawlers.py
@@ -1,8 +1,6 @@
 import math
 import tkinter as tk
 
-# https://www.takealot.com/osah-drypak-18l-backpack-orange/PLID56719248?colour_variant=Yellow
-
 class lead_crawler:
     def __init__(self, scene, x, y, a, w, l, mf, mt):
         self.x_position = x
@@ -27,10 +25,34 @@ class lead_crawler:
             self.x_position + w/2,      #x3 
             self.y_position + l/2,      #y3 
             self.x_position - w/2,      #x4 
-            self.y_position + l/2]      #y5 
+            self.y_position + l/2]      #y5
+
+        # set crawlers initial rotation
+        new_coordinates = []
+        delta_angle = self.angle
+
+        for z in range(int(len(self.coordinates)/2)):
+            x = self.coordinates[z*2]
+            y = self.coordinates[z*2+1]
+            r = math.sqrt((x - self.x_position)**2 + (y - self.y_position)**2)
+            q = math.degrees(abs(math.acos((x - self.x_position)/r))) 
+
+            if x >= self.x_position and y >= self.y_position:       # quadrant 1
+                qn = math.radians(q - delta_angle)
+            elif x <= self.x_position and y >= self.y_position:     # quadrant 2
+                qn = math.pi - math.radians((180 - q + delta_angle))
+            elif x <= self.x_position and y <= self.y_position:     # quadrant 3
+                qn = math.pi + math.radians((180 - q - delta_angle))
+            else: qn = -math.radians((q + delta_angle))             # quadrant 4
+
+            new_coordinates.append(r*math.cos(qn) + self.x_position)
+            new_coordinates.append(r*math.sin(qn) + self.y_position)
 
         self.crawler = scene.canvas.create_polygon(
-            self.coordinates, outline = "black", fill = "green", width=2)        
+            self.coordinates, outline = "black", fill = "green", width=2)
+
+        self.coordinates = new_coordinates
+        scene.canvas.coords(self.crawler, *new_coordinates)        
 
     def move_forward(self, scene, dt):
         delta_forward = dt*self.max_forward_speed
@@ -45,14 +67,16 @@ class lead_crawler:
             if x1 > x2:
                 for cord in range(len(self.coordinates)):
                     if cord % 2 == 0:
-                        self.coordinates[cord] -= delta_x
+                        self.coordinates[cord] -= delta_forward
                 scene.canvas.coords(self.crawler, *self.coordinates)
+                self.y_position -= delta_forward
                 return
             else:
                 for cord in range(len(self.coordinates)):
                     if cord % 2 == 0:
-                        self.coordinates[cord] += delta_x
+                        self.coordinates[cord] += delta_forward
                 scene.canvas.coords(self.crawler, *self.coordinates)
+                self.y_position += delta_forward
                 return
 
         elif x1 == x2:
@@ -61,22 +85,30 @@ class lead_crawler:
                     if cord % 2 != 0:
                         self.coordinates[cord] += delta_forward
                 scene.canvas.coords(self.crawler, *self.coordinates)
+                self.x_position += delta_forward
                 return
             else:
                 for cord in range(len(self.coordinates)):
                     if cord % 2 != 0:
                         self.coordinates[cord] -= delta_forward
                 scene.canvas.coords(self.crawler, *self.coordinates)
+                self.x_position -= delta_forward
                 return
 
+        # msitake is somehere here
+
         q = math.atan((y1-y2)/(x1-x2))
-        delta_x = delta_forward*math.cos(q)
-        delta_y = delta_forward*math.sin(q)
+        delta_x = abs(delta_forward*math.cos(q))
+        delta_y = abs(delta_forward*math.sin(q))
 
         for cord in range(len(self.coordinates)):
             if cord % 2 == 0:
-                self.coordinates[cord] += delta_x
-            else: self.coordinates[cord] += delta_y
+                self.coordinates[cord] -= delta_x
+            else: self.coordinates[cord] -= delta_y
+
+        # update centre position
+        self.x_position -= delta_x
+        self.y_position -= delta_y
 
         scene.canvas.coords(self.crawler, *self.coordinates)
 
@@ -93,14 +125,16 @@ class lead_crawler:
             if x1 < x2:
                 for cord in range(len(self.coordinates)):
                     if cord % 2 == 0:
-                        self.coordinates[cord] -= delta_x
+                        self.coordinates[cord] -= delta_forward
                 scene.canvas.coords(self.crawler, *self.coordinates)
+                self.y_position -= delta_forward
                 return
             else:
                 for cord in range(len(self.coordinates)):
                     if cord % 2 == 0:
-                        self.coordinates[cord] += delta_x
+                        self.coordinates[cord] += delta_forward
                 scene.canvas.coords(self.crawler, *self.coordinates)
+                self.y_position += delta_forward
                 return
 
         elif x1 == x2:
@@ -109,22 +143,30 @@ class lead_crawler:
                     if cord % 2 != 0:
                         self.coordinates[cord] += delta_forward
                 scene.canvas.coords(self.crawler, *self.coordinates)
+                self.x_position += delta_forward
                 return
             else:
                 for cord in range(len(self.coordinates)):
                     if cord % 2 != 0:
                         self.coordinates[cord] -= delta_forward
                 scene.canvas.coords(self.crawler, *self.coordinates)
+                self.x_position -= delta_forward
                 return
 
+        # mistake is somewhere here
+
         q = math.atan((y1-y2)/(x1-x2))
-        delta_x = delta_forward*math.cos(q)
-        delta_y = delta_forward*math.sin(q)
+        delta_x = abs(delta_forward*math.cos(q))
+        delta_y = abs(delta_forward*math.sin(q))
 
         for cord in range(len(self.coordinates)):
             if cord % 2 == 0:
-                self.coordinates[cord] -= delta_x
-            else: self.coordinates[cord] -= delta_y
+                self.coordinates[cord] += delta_x
+            else: self.coordinates[cord] += delta_y
+
+        # update center postion
+        self.x_position += delta_x
+        self.y_position += delta_y
 
         scene.canvas.coords(self.crawler, *self.coordinates)
 
